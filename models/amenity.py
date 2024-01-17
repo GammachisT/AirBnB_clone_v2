8c592fdd192c8804826f0fffb12fa0f561028074
GammachiT <gammachistesfaye@gmail.com>
2024-01-17 16:40:41 +0000

    Airbnb clone v2

--- models/amenity.py
+++ models/amenity.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
 """ Amenity Module for HBNB project """
-import os
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-
+import models
 from models.base_model import BaseModel, Base
 
 
 class Amenity(BaseModel, Base):
     """Represents an amenity data set."""
     __tablename__ = 'amenities'
-    name = Column(
-        String(128), nullable=False
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+    name = Column(String(128), nullable=False)
+    # place_amenities = relationship('Place', secondary='place_amenity')
